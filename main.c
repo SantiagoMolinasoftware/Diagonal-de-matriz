b357678c68b5b83f3f20e46ec9ecaf0d7099ba77
Santiago Molina <santiago.molina.olivo@udla.edu.ec>
2023-11-22 16:13:40 -0500

    2do commit: creacion de otras funciones

--- main.c
+++ main.c
@@ -8,3 +8,22 @@ void inicializarMatriz(int matriz[][100], int filas, int columnas) {
         }
     }
 }
+
+// Función para asignar unos en la diagonal de la matriz
+void asignarUnosDiagonal(int matriz[][100], int filas, int columnas) {
+    int minSize = (filas < columnas) ? filas : columnas; // Determinar el tamaño mínimo para evitar desbordamientos
+    for (int i = 0; i < minSize; i++) {
+        matriz[i][i] = 1;
+    }
+}
+
+// Función para imprimir la matriz
+void imprimirMatriz(int matriz[][100], int filas, int columnas) {
+    printf("Matriz Resultante:\n");
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+}
