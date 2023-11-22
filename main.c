763a9a4c602842e5a7ce809b9c1a3222e464dbfe
Santiago Molina <santiago.molina.olivo@udla.edu.ec>
2023-11-22 16:12:46 -0500

    1er commit: inicializacion de codigo y primera funcion

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+// Función para inicializar la matriz con ceros
+void inicializarMatriz(int matriz[][100], int filas, int columnas) {
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+            matriz[i][j] = 0;
+        }
+    }
+}
